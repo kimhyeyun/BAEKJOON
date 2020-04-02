3228c014fbe0b6805b6e49cc12f97e4b9cd9bacd
kimhyeyun <cqqwer@gmail.com>
2020-04-03 01:06:39 +0900

    같은 알파벳은 한번이상 못지나가는 조건에서 갈 수 있는 최대거리 구하기

--- DFS/DFS/1987.cpp
+++ DFS/DFS/1987.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
-#include<stdio.h>
-#include<vector>
+#include<stdio.h>	
+#include<algorithm>
 
 using namespace std;
 
@@ -8,22 +8,43 @@ using namespace std;
 
 int R, C;
 char board[MAX][MAX];
-bool visited[MAX][MAX];
-vector<char> vschar;
+bool alphabet[26] = { false, }; //alphabet 지나갔나?
+
+int dir[][2] = { {0,-1},{0,1},{-1,0},{1,0} };
+int cnt = 0;
 
 //dfs
-int checkCnt() {
-	visited[0][0] = true;
+void checkCnt(int startx,int starty,int count) {
+	cnt = max(cnt, count); //최대값 구하는 것
+
+
+	for (int i = 0; i < 4; i++) {
+		int nx = startx + dir[i][0];
+		int ny = starty + dir[i][1];
+
+		if (nx >= 0 && nx < R && ny >= 0 && ny < C) {
+			int k = (int)board[nx][ny] - 65; 
+			if (!alphabet[k]) { //안지나갔으면
+				alphabet[k] = true; //지나갔다하고
+				checkCnt(nx, ny, count + 1); //다시 dfs
+				alphabet[k]=false; //한 dfs가 끝나면 그 alphabet은 해제, 다른 방향으로 지나갈수 있어야하므로 
+			}
+		}
+	}
+	return;
 }
 
 int main() {
 	cin >> R >> C;
 	
 	for (int i = 0; i < R; i++) {
-		for (int j = 0; j < C; j++) {
-			cin >> board[i][j];
-		}
+		/*for (int j = 0; j < C; j++) {
+			scanf_s("%1c", &board[i][j]);
+		}*/
+		cin >> board[i];
 	}
 
-	int result = checkCnt();
+	alphabet[board[0][0] - 65] = true;
+	checkCnt(0,0,1);
+	printf("%d\n", cnt);
 }
