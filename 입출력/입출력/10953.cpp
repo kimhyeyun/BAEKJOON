71104b58643ec1c69545b77728c076b6f414969f
kimhyeyun <cqqwer@gmail.com>
2020-04-06 20:55:18 +0900

    숫자입력 사이 , 입력

---
+++ 입출력/입출력/10953.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+int main() {
+	int A, B, T;
+	char a;
+
+	for (int test_case = 1; test_case <= T; test_case++) {
+		cin >> A >> a >> B;
+		printf("%d\n", A + B);
+	}
+
+
+}
